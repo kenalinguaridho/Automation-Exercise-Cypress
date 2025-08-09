7054b87147531f72e816fd16052d85fe7bf6d5f3
Muhammad Ridho <muhammadridho.mail@gmail.com>
2025-08-10 04:01:28 +0700

    add TC-008

---
+++ cypress/e2e/Products.cy.js
@@ -0,0 +1,22 @@
+import '../support/homePage.js'
+import '../support/Products.js'
+import '../support/components/navbar.js'
+import '../support/URL.js'
+
+describe('Products Feature', () => {
+
+    beforeEach(() => {
+        cy.viewport(1280, 720)
+        cy.HomePageVisibility()
+        cy.ClickNavbarMenu('Products')
+        
+    })
+
+    it('TC-008-Verify All Products and product detail page', async () => {
+        const id = 1
+        cy.VerifyAllProductsVisibility()
+        cy.ClickViewProduct(id)
+        cy.VerifyURL(`/product_details/${id}`)
+        cy.VerifyProductDetails(id)
+    })
+})
