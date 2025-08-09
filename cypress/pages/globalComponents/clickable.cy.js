40617d28641adbdc5718a7e57dacb6fa37f3dd87
Muhammad Ridho <muhammadridho.mail@gmail.com>
2025-08-10 04:02:27 +0700

    add clickable class to refactor clickable components

---
+++ cypress/pages/globalComponents/clickable.cy.js
@@ -0,0 +1,23 @@
+export default class Clickable {
+
+    constructor (selector) {
+        this.selector = selector
+    }
+
+    clickFromList(option) {
+        const list = cy.get(this.selector)
+
+        if (option === 'first' || option === 'last') {
+            list[option]().click()  
+        } else if (typeof option === 'number') {
+            list.eq(option-1).click()
+        } else {
+            throw new Error('Invalid option for button selection')
+        }
+    }
+
+    clickByText(text) {
+        cy.get(this.selector).contains(text).click()
+    }
+
+}
