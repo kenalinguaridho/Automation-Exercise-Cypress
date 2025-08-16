6c0880c02f17c4e41ba486c93a3fbb373a9991da
Muhammad Ridho <muhammadridho.mail@gmail.com>
2025-08-17 00:45:01 +0700

    add some commands related to adding product into cart

--- cypress/support/Products.js
+++ cypress/support/Products.js
@@ -1,4 +1,6 @@
 import Clickable from '../pages/globalComponents/clickable.cy'
+import Hover from '../pages/globalComponents/hover.cy'
+import Input from '../pages/globalComponents/input.cy'
 import LeftSideBar from '../pages/globalComponents/leftSideBar.cy'
 import Products from '../pages/Products.cy'
 
@@ -30,4 +32,23 @@ Cypress.Commands.add('ClickSearchButton', () => {
 Cypress.Commands.add('VerifySearchedProducts', (name) => {
     Products.verifySearchProductsPage()
     Products.verifySearchedProducts(name)
+})
+
+Cypress.Commands.add('AddProductToCart', (option) => {
+    new Hover('.product-image-wrapper > .single-products').hoverFromList(option)
+    new Clickable('.product-overlay > .overlay-content > a').clickFromList(option)
+})
+
+Cypress.Commands.add('ClickContinueShopping', () => {
+    new Clickable('.modal-content > .modal-footer > button').clickButton()
+})
+
+Cypress.Commands.add('ProductToCart', () => {
+    new Clickable('.modal-body > p > a').clickButton()
+})
+
+Cypress.Commands.add('AddSingleProductToCart', (text) => {
+    new Input('input[id="quantity"]').clear()
+    new Input('input[id="quantity"]').typeInput(text)
+    new Clickable('span > button').clickButton()
 })
